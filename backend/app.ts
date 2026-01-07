6e6af62bd132392525ee24cc2bb79a88b7355dd7
chrisson <chrisson543@gmail.com>
2026-01-07 12:13:42 +0100

    debugging

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,30 @@
-// app.ts
+import 'dotenv/config';
 import express from 'express';
+import UserRoutes from './routes/UserRoutes'
+import PostRoutes from './routes/PostRoutes'
+import LLMRoutes from './routes/LLMRoutes'
+import AuthRoutes from './routes/AuthRoutes';
+import cors from "cors";
+import { auth } from './middleware/auth'
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.get('/health', (_req, res) => {
-  res.json({ ok: true });
-});
+app.use(cors({
+  origin: process.env.FRONTEND_ORIGIN,
+  credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-export default app;
+app.use('/auth', AuthRoutes)
+
+app.use(auth)
+
+app.use('/users', UserRoutes)
+app.use('/posts', PostRoutes)
+app.use('/llm', LLMRoutes)
+
+
+export default app
