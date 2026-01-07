5da9a5105b3981e74797c5b4861ed7ea629ea7f1
chrisson <chrisson543@gmail.com>
2026-01-07 11:48:13 +0100

    updated gitignore

--- backend/controllers/test.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcrypt");
-async function getHash(){   
-    const password_hash = await bcrypt.hash('Kitchen543.', Number(process.env.SALT_ROUNDS))
-    console.log(password_hash)
-}
-
-getHash()
